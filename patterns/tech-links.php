d5ef5d0291a2ccfda86ec7e1137cf8564b00e53b
Aleksey Krivoshein <a.krivoshein@gmail.com>
2026-04-14 09:50:56 +0300

    Switch default theme palette to light and tune contacts pattern

--- patterns/tech-links.php
+++ patterns/tech-links.php
@@ -4,19 +4,20 @@
  * Slug: drslon-blog/tech-links
  * Categories: featured
  * Inserter: yes
+ * Description: Curated links block for sidebar or standalone pages.
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:heading {"level":3} -->
-	<h3>Полезные ссылки</h3>
+<!-- wp:group {"style":{"spacing":{"blockGap":"12px","padding":{"top":"20px","right":"20px","bottom":"20px","left":"20px"}},"border":{"width":"1px","color":"var:preset|color|border","radius":"12px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-color:var(--wp--preset--color--border);border-width:1px;border-radius:12px;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px">
+	<!-- wp:heading {"level":3,"fontSize":"sm"} -->
+	<h3 class="has-sm-font-size">Полезные ссылки</h3>
 	<!-- /wp:heading -->
 
 	<!-- wp:list -->
 	<ul>
-		<li><a href="https://krivoshein.site">Блог</a></li>
-		<li><a href="https://t.me/DrSlon">Telegram</a></li>
-		<li><a href="https://github.com/A-Krivoshen">GitHub</a></li>
+		<li><a href="https://wordpress.org/documentation/">Документация WordPress</a></li>
+		<li><a href="https://docs.docker.com/">Документация Docker</a></li>
+		<li><a href="https://nginx.org/en/docs/">Документация Nginx</a></li>
 	</ul>
 	<!-- /wp:list -->
 </div>
