6fadfa5c65e0e957af9f25d75efd426a548c4eb5
Aleksey Krivoshein <a.krivoshein@gmail.com>
2026-04-14 20:21:11 +0300

    feat: category.html + настройка количества постов в рубриках

--- functions.php
+++ functions.php
@@ -5,3 +5,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once get_template_directory() . '/inc/legacy-compat.php';
 require_once get_template_directory() . '/inc/legacy-shortcodes.php';
+
+/* =============================================
+   Настройка количества постов на страницах рубрик
+   ============================================= */
+add_action( 'customize_register', 'drslon_customize_category_posts' );
+function drslon_customize_category_posts( $wp_customize ) {
+
+    $wp_customize->add_section( 'drslon_category_section', array(
+        'title'    => 'Страницы рубрик',
+        'priority' => 130,
+    ));
+
+    $wp_customize->add_setting( 'drslon_posts_per_category', array(
+        'default'           => 12,
+        'sanitize_callback' => 'absint',
+    ));
+
+    $wp_customize->add_control( 'drslon_posts_per_category', array(
+        'label'       => 'Количество постов на странице рубрики',
+        'section'     => 'drslon_category_section',
+        'type'        => 'number',
+        'input_attrs' => array(
+            'min'  => 1,
+            'max'  => 48,
+            'step' => 1,
+        ),
+    ));
+}
