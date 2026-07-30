5c4edb67ad332419327fcbd7c47f2842b54ab2dc
Aleksey Krivoshein <a.krivoshein@gmail.com>
2026-07-30 19:03:44 +0300

    chore: bump KRV assistant embed to v=20260730j

--- inc/krv-assistant.php
+++ inc/krv-assistant.php
@@ -24,7 +24,7 @@ function drslon_krv_assistant_footer(): void {
 		return;
 	}
 
-	$src = 'https://support.krivoshein.site/widget/krv-assistant.js?v=20260730i';
+	$src = 'https://support.krivoshein.site/widget/krv-assistant.js?v=20260730j';
 	$api = 'https://support.krivoshein.site/api/v1';
 
 	printf(
